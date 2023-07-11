283edc27b1ecc89b7ed84765085d0bcaee8d4097
MatheusVinicius <matheusvinicius123466@gmail.com>
2023-07-11 17:45:47 -0300

    chore: transaction table done

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,36 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLighting, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transaction() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLighting variant="income" >R$ 12.000,00</PriceHighLighting>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width='50%'>hamburguer</td>
+              <td>
+                <PriceHighLighting variant="outcome" > - R$ 59,00</PriceHighLighting>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1150px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${(props) => props.theme["gray-700"]};
+
+    &:first-child {
+      border-bottom-left-radius: 6px;
+      border-top-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-bottom-right-radius: 6px;
+      border-top-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHighLightingProps {
+  variant: 'income' | 'outcome'
+}
+
+export const PriceHighLighting = styled.span<PriceHighLightingProps>`
+  color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]}
+`;
