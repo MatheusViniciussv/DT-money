f5fa8c74cf3c2f79383f159f2952aaa477b457b6
MatheusVinicius <matheusvinicius123466@gmail.com>
2023-07-12 11:39:34 -0300

    searchForm done / create an dialog transaction

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighLighting, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transaction() {
@@ -9,6 +10,8 @@ export function Transaction() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
